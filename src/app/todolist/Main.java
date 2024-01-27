7e31ad2ad1bd828bb56b5f7de328e65945514602
f2lpOliveira <contato@fellipefreire.com.br>
2024-01-27 07:33:37 -0300

    menu-iteracao_1.0.2.java

--- src/app/todolist/Main.java
+++ src/app/todolist/Main.java
@@ -1,5 +1,7 @@
 package app.todolist;
 
+import menu.MenuIterativo;
+
 public class Main {
     public static void main(String[] args) {
         // Instancia a classe que contém o menu iterativo
--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,62 +1,45 @@
 package menu;
 
-import tarefa.CriarTarefa;
-import tarefa.AtualizarTarefa;
-import tarefa.ListarTarefas;
-import tarefa.DeletarTarefa;
-
 import java.util.Scanner;
 
-public class Menu {
-    private CriarTarefa criarTarefa;
-    private AtualizarTarefa atualizarTarefa;
-    private ListarTarefas listarTarefas;
-    private DeletarTarefa deletarTarefa;
-
-    public Menu() {
-        criarTarefa = new CriarTarefa();
-        atualizarTarefa = new AtualizarTarefa();
-        listarTarefas = new ListarTarefas();
-        deletarTarefa = new DeletarTarefa();
-    }
-
-    public void executarMenu() {
+public class MenuIterativo {
+    // Método para apresentar o menu
+    public static void apresentacao() {
         Scanner scanner = new Scanner(System.in);
         int escolha;
 
         do {
-            System.out.println("===== Menu de Tarefas =====");
-            System.out.println("1. Criar Tarefa");
-            System.out.println("2. Listar Tarefas");
-            System.out.println("3. Atualizar Tarefa");
-            System.out.println("4. Excluir Tarefa");
-            System.out.println("5. Sair");
-            System.out.print("Escolha uma opção: ");
+            System.out.println("----- Menu -----");
+            System.out.println("1. Criar nova tarefa");
+            System.out.println("2. Listar tarefas");
+            System.out.println("3. Excluir tarefas");
+            System.out.println("4. Sair");
+            System.out.print("Escolha uma opção (1-4): ");
 
+            // Lê a escolha do usuário
             escolha = scanner.nextInt();
+            scanner.nextLine(); // Consumir a quebra de linha pendente
 
+            // Executa a opção escolhida
             switch (escolha) {
                 case 1:
-                    criarTarefa.executarCriacao();
+                    Tarefa.criarTarefa();
                     break;
                 case 2:
-                    listarTarefas.executarListagem();
+                    Tarefa.listarTarefas();
                     break;
                 case 3:
-                    atualizarTarefa.executarAtualizacao();
+                    Tarefa.excluirTarefa();
                     break;
                 case 4:
-                    deletarTarefa.executarDelecao();
-                    break;
-                case 5:
-                    System.out.println("Saindo do Gerenciador de Tarefas. Até mais!");
+                    System.out.println("Saindo do programa. Até mais!");
                     break;
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
+                    break;
             }
-
-        } while (escolha != 5);
-
-        scanner.close();
+        } while (escolha != 4);
     }
+
 }
+
