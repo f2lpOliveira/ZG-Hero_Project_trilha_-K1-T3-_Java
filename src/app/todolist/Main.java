b8b60a63b93d82e1645c131a90b421c760858f8f
f2lpOliveira <contato@fellipefreire.com.br>
2024-02-15 07:33:54 -0300

    alarmeTarefa_1.0.1.java

--- src/app/todolist/Main.java
+++ src/app/todolist/Main.java
@@ -4,7 +4,6 @@ import menu.MenuIterativo;
 
 public class Main {
     public static void main(String[] args) {
-        // Instancia a classe que contém o menu iterativo
         MenuIterativo.apresentacao();
     }
 }
